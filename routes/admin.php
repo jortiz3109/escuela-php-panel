8614cee07c7e5993019dbbddd83a796a354c3525
Juango <2gmurillo@gmail.com>
2021-09-24 12:01:14 -0500

    Revert "Feature/ep 15 integrate adminlte template"

--- routes/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
-
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
