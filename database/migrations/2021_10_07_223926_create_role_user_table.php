8e3d9e23fc27f00962b89acdc7ba40609323737e
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-07 17:49:12 -0500

    feat: Add migration to assign a role to a user

---
+++ database/migrations/2021_10_07_223926_create_role_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleUserTable extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->timestamp('created_at')->nullable();
+
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->unique(['user_id', 'role_id']);
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('role_user');
+    }
+}
