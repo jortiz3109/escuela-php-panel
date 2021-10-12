2445a29be440f55b13df1d9d04db6ffb193cd3b1
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-12 16:52:19 -0500

    feat: Add translation for permissions module

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -1,4 +1,10 @@
 <table>
+    <theaders>
+        <tr>
+            <th>@lang('permissions.fields.name')</th>
+            <th>@lang('permissions.fields.description')</th>
+        </tr>
+    </theaders>
     <tbody>
     @foreach($permissions as $permission)
         <tr>
