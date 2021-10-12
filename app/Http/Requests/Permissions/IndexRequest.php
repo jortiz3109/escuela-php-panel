e6e03e1ae7a0de1f42084f720e93d60547fcbfd5
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-12 16:43:36 -0500

    feat: Add permission model

---
+++ app/Http/Requests/Permissions/IndexRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Permissions;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'filters' => ['filled', 'array'],
+            'filters.name' => ['filled', 'string', 'min:2', 'max:125'],
+        ];
+    }
+}
