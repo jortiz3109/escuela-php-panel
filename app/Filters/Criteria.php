f6bd2216ee7025d33b9cf4bc14ec87bca865d680
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-12 16:42:53 -0500

    feat: add model filters

---
+++ app/Filters/Criteria.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filters;
+
+class Criteria
+{
+    private string|array $value;
+
+    public function __construct(string|array $value)
+    {
+        $this->value = $value;
+    }
+
+    public function value(): string|array
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        if (is_array($this->value)) {
+            return implode(',', $this->value);
+        }
+
+        return $this->value;
+    }
+}
