8d0e52a08e4c483c83510a43d3e16e52d54ed69d
RobertJDevOP <rcjimenez35@gmail.com>
2021-10-20 22:20:28 -0500

    fix: fix issue of ModelFilters users

--- app/Filters/ModelFilters/UserFilters.php
+++ app/Filters/ModelFilters/UserFilters.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace App\Filters\ModelFilters;
+use App\Filters\Conditions\Email;
+use App\Filters\Conditions\EnabledAt;
+use App\Filters\Conditions\Name;
+use App\Filters\Filter;
+use App\Models\User;
 
-class UserFilters
+class UserFilters extends Filter
 {
+    protected string $model = User::class;
+    protected array $applicableConditions = [
+        'name' => Name::class,
+        'email' => Email::class,
+        'enabled_at' => EnabledAt::class,
+    ];
 
+    protected function select(): Filter
+    {
+        $this->query->select(['id', 'name', 'email', 'created_at', 'updated_at', 'enabled_at']);
+        return $this;
+    }
 }
+
