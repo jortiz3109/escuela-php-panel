5f34be438cdba76c183e12ba9d702f014efba136
RobertJDevOP <rcjimenez35@gmail.com>
2021-10-20 21:02:30 -0500

    feat: ViewModel for users

--- app/ViewModels/Users/IndexViewModel.php
+++ app/ViewModels/Users/IndexViewModel.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace App\ViewModels\Users;
+use App\ViewModels\Concerns\HasCollection;
+use App\ViewModels\ViewModel;
 
-class IndexViewModel
+class IndexViewModel extends ViewModel
 {
+    use HasCollection;
 
+    protected function buttons(): array
+    {
+        return [];
+    }
+
+    protected function title(): string
+    {
+        return trans('users.titles.index');
+    }
+
+    protected function data(): array
+    {
+        return [
+            'users' => $this->collection,
+        ];
+    }
 }
