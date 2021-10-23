3938765c3095d4d5896c0a59d37ba5383574c452
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-10-23 08:26:26 -0500

    fix: remove references to missing routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,7 +89,7 @@
     @if (Route::has('login'))
         <div class="top-right links">
             @if (Auth::check())
-                <a href="{{ url('/home') }}">Home</a>
+                <a href="{{ url('/') }}">Home</a>
             @else
                 <a href="{{ url('/login') }}">Login</a>
             @endif
