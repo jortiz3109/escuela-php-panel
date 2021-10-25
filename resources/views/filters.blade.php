70368607b422cb43a0f22710c754fb27156d245c
John Edisson Ortiz <j.ortiz3109@gmail.com>
2021-10-25 15:52:38 -0500

    feat: Add filters view

---
+++ resources/views/filters.blade.php
@@ -0,0 +1,21 @@
+<b-collapse class="card" animation="slide" :open="{{ request()->has('filters') ? 'true' : 'false' }}" v-cloak>
+    <template #trigger="props">
+        <div
+            class="card-header"
+            role="button">
+            <p class="card-header-title">Filters</p>
+            <a class="card-header-icon"><b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon></a>
+        </div>
+    </template>
+
+    <div class="card-content">
+        <form action="{{ url()->current() }}" method="GET">
+            @foreach($filters as $filter => $value)
+                @include("partials.filters.{$filter}", compact('value'))
+            @endforeach
+            <b-button type="is-link" native-type="submit" icon-left="magnify">@lang('filters.find')</b-button>
+            <b-button tag="a" type="is-link" href="{{ url()->current() }}" icon-left="eraser">@lang('filters.clear')</b-button>
+        </form>
+    </div>
+</b-collapse>
+<hr>
