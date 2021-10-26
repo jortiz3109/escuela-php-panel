d57981cf59594f6aa08ea6dc5cdb96dccc8382bc
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-10-25 20:21:09 -0500

    fix: add missing filters property

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,6 +11,7 @@ class DashboardController extends Controller
                 'title' => 'Dashboard',
             ],
             'buttons' => [],
+            'filters' => [],
         ]);
     }
 }
