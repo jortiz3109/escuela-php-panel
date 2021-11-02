6cf277f2aa3b8f9247c71ec5f7608e42d25c3bdc
Jennifer <jeante05@gmail.com>
2021-11-02 16:56:19 -0500

    fix: migrate add create update user

--- database/migrations/2021_10_24_224610_add_create_update_user.php
+++ database/migrations/2021_10_24_224610_add_create_update_user.php
@@ -4,9 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnUser extends Migration
+class AddCreateUpdateUser extends Migration
 {
-
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
