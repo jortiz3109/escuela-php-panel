3714e7dc40b96ca132d89f6e98ec968108ad9fe9
Jennifer <jeante05@gmail.com>
2021-11-02 16:00:01 -0500

    fix: comment request user

--- app/Http/Requests/Register/UserCreateRequest.php
+++ app/Http/Requests/Register/UserCreateRequest.php
@@ -17,11 +17,6 @@ class UserCreateRequest extends FormRequest
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
