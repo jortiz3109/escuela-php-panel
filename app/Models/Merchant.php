c4c38985a042c9cbb8c4ce87fe6b0e132c4f78bd
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-04 23:20:01 -0500

    feat: add merchant model and factory

---
+++ app/Models/Merchant.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Merchant extends Model
+{
+    use HasFactory;
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+}
---
+++ database/factories/MerchantFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MerchantFactory extends Factory
+{
+    // protected string $model = Merchant::class;
+
+    public function definition(): array
+    {
+        return [
+            'uuid' => $this->faker->uuid(),
+            'document' => $this->faker->unique()->bothify('###########'),
+            'name' => $this->faker->company(),
+            'brand' => $this->faker->bs(),
+            'url' => $this->faker->url(),
+            'logo' => $this->faker->image(null, 100, 100),
+        ];
+    }
+}
