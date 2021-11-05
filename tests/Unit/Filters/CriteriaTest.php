9ed14d9b4f5e3a8c894a32abd5d7f75dd8fdb809
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-05 09:38:36 -0500

    style: PHP CS FIXER

--- tests/Unit/Filters/CriteriaTest.php
+++ tests/Unit/Filters/CriteriaTest.php
@@ -16,12 +16,12 @@ class CriteriaTest extends TestCase
     public function test_it_returns_an_empty_string_when_received_a_null_value()
     {
         $criteria = new Criteria(null);
-        $this->assertSame('', (string) $criteria);
+        $this->assertSame('', (string)$criteria);
     }
 
     public function test_it_returns_a_comma_separate_string_when_received_an_array()
     {
         $criteria = new Criteria(['A' , 'B', 'C']);
-        $this->assertSame('A,B,C', (string) $criteria);
+        $this->assertSame('A,B,C', (string)$criteria);
     }
 }
