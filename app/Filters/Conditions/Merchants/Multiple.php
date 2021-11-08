458f77b0bbcfbe180c834bd8509deac8db227e1a
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-08 17:26:40 -0500

    fix: remove instruction to debug

--- app/Filters/Conditions/Merchants/Multiple.php
+++ app/Filters/Conditions/Merchants/Multiple.php
@@ -5,14 +5,11 @@ namespace App\Filters\Conditions\Merchants;
 use App\Filters\Condition;
 use App\Filters\Criteria;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Log;
 
 class Multiple extends Condition
 {
     public static function append(Builder $query, Criteria $criteria): void
     {
-        Log::info("Pasó por aquí");
-        
         $query->where('merchants.name', 'like', "%{$criteria}%")
             ->orWhere('merchants.brand', 'like', "%{$criteria}%")
             ->orWhere('merchants.document', 'like', "%{$criteria}%")
