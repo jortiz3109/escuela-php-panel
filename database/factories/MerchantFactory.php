7d691d710f876f18874a31d0650d55caf46100b3
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-12 16:37:35 -0500

    feat: load relationships in MerchantFactory

--- database/factories/MerchantFactory.php
+++ database/factories/MerchantFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Country;
+use App\Models\Currency;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MerchantFactory extends Factory
@@ -9,12 +11,20 @@ class MerchantFactory extends Factory
     public function definition(): array
     {
         return [
-            'uuid'     => $this->faker->uuid(),
+            'uuid' => $this->faker->uuid(),
             'document' => $this->faker->unique()->bothify('###########'),
-            'name'     => $this->faker->company(),
-            'brand'    => $this->faker->bs(),
-            'url'      => $this->faker->url(),
-            'logo'     => $this->faker->image(null, 100, 100),
+            'name' => $this->faker->company(),
+            'brand' => $this->faker->bs(),
+            'url' => $this->faker->url(),
+            'logo' => $this->faker->image(null, 100, 100),
+
+            'country_id'  => Country::firstOrCreate(
+                Country::factory()->make()->toArray()
+            )->id,
+
+            'currency_id' => Currency::firstOrCreate(
+                Currency::factory()->make()->toArray()
+            )->id,
         ];
     }
 }
