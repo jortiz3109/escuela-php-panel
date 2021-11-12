e3eefb0d01185b6a6d69ad7048ae3cc1fe8bd096
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-12 08:39:39 -0500

    feat: payment methods migration, model and seeder

---
+++ database/migrations/2021_11_12_133406_create_payment_methods_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentMethodsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('logo', 255);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+}
