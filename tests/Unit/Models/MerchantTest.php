e318db075127691893106d1149c5d25cb4c6d947
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-14 16:17:47 -0500

    test: add merchant tests to improve coverage

---
+++ tests/Unit/Models/MerchantTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Country;
+use App\Models\Currency;
+use App\Models\Merchant;
+use App\Models\Transaction;
+use Tests\TestCase;
+
+class MerchantTest extends TestCase
+{
+    public function test_belongs_to_a_country(): void
+    {
+        $merchant = Merchant::factory()->create();
+        $this->assertInstanceOf(Country::class, $merchant->country);
+    }
+
+    public function test_belongs_to_a_currency(): void
+    {
+        $merchant = Merchant::factory()->create();
+        $this->assertInstanceOf(Currency::class, $merchant->currency);
+    }
+
+    public function test_has_many_transactions(): void
+    {
+        $merchant = Merchant::factory()->create();
+        $transaction = Transaction::factory()->for($merchant, 'merchant')->create();
+
+        $this->assertTrue($merchant->transactions->contains($transaction));
+        $this->assertEquals(1, $merchant->transactions->count());
+        $this->assertInstanceOf(
+            'Illuminate\Database\Eloquent\Collection',
+            $merchant->transactions
+        );
+    }
+}
