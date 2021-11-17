b96814fb5b1d2f0a78e6e02b1e9a1a7bdbb86f04
Alejandro Silva Berbesí <me@asilvabe.dev>
2021-11-16 21:18:52 -0500

    style: remove empty method

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -9,11 +9,6 @@ use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
-
     public function boot(): void
     {
         View::composer('merchants.index', CountryComposer::class);
