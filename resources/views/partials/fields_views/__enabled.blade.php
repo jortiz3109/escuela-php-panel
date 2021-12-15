551af3992bcb05df108ca329e4a1df4a8db19ec3
Alexander Camilo <fredy.camilo@evertecinc.com>
2021-12-14 19:06:31 -0500

    refactor: resolve code smells

--- resources/views/partials/fields_views/__enabled.blade.php
+++ resources/views/partials/fields_views/__enabled.blade.php
@@ -2,9 +2,9 @@
     <th scope="row">{{ $label }}</th>
     <td>
         @if($value)
-            <b-tag rounded class="is-success"><span><i class="fas fa-check-circle"></i><span>{{ trans('common.fields.enabled') }}</b-tag>
+            <b-tag rounded class="is-success"><span><em class="fas fa-check-circle"></em><span>{{ trans('common.fields.enabled') }}</b-tag>
         @else
-            <b-tag rounded class="is-danger"><span class="icon"><i class="fas fa-ban"></i></span>{{ trans('common.fields.disabled') }}</b-tag>
+            <b-tag rounded class="is-danger"><span class="icon"><em class="fas fa-ban"></em></span>{{ trans('common.fields.disabled') }}</b-tag>
         @endif
     </td>
 </tr>
