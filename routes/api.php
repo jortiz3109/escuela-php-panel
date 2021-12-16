0fc966938a7a6372f1eac61d028cc0d0b27251da
Juan David Zea <vikzea@gmail.com>
2021-12-16 08:08:49 -0500

    refactor: Run php fixer

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
