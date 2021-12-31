b55291e69daab4bfa56a9460e67c276e874a5ccb
Alexander Camilo <fredy.camilo@evertecinc.com>
2021-12-31 11:10:41 -0500

    refactor: add resource to show transactions and use display components

---
+++ app/Http/Resources/Transactions/TransactionShowResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Transactions;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionShowResource extends JsonResource
+{
+    public function toArray($request = null): array
+    {
+        return [
+            'merchant' => $this->resource->merchant->name,
+            'reference' => $this->resource->reference,
+            'payment_method' => $this->resource->paymentMethod->logo,
+            'card_number' => $this->resource->card_number,
+            'currency' => $this->resource->currency->name . ' (' . $this->resource->currency->alphabetic_code . ')',
+            'total_amount' => $this->resource->total_amount,
+            'status' => $this->resource->status,
+            'ip_address' => $this->resource->ip_address,
+            'executed_at' => $this->resource->executed_at,
+            'geolocation' => $this->resource->ip_address,
+            'payer_name' => $this->resource->payer->name,
+            'payer_email' => $this->resource->payer->email,
+            'buyer_name' => $this->resource->buyer->name,
+            'buyer_email' => $this->resource->buyer->email,
+        ];
+    }
+}
