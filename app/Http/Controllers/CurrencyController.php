a28cec93ca096118b8bbd56e528515887a9a4ab0
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-01-03 23:30:01 -0500

    feat: add currencies filters validation

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Currencies\IndexRequest;
 use App\Models\Currency;
 use App\ViewModels\Currencies\CurrencyIndexViewModel;
 use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CurrencyController extends Controller
@@ -13,7 +13,7 @@ class CurrencyController extends Controller
     /**
      * @throws BindingResolutionException
      */
-    public function index(Request $request, CurrencyIndexViewModel $viewModel): View
+    public function index(IndexRequest $request, CurrencyIndexViewModel $viewModel): View
     {
         $currencies = Currency::filter($request->input('filters', []))->paginate();
 
--- app/Http/Requests/Currencies/IndexRequest.php
+++ app/Http/Requests/Currencies/IndexRequest.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Requests\Currencies;
 
-class IndexRequest
-{
+use Illuminate\Foundation\Http\FormRequest;
 
+class IndexRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'filters' => ['bail', 'filled', 'array'],
+            'filters.name' => ['bail', 'filled', 'string', 'min:2', 'max:125'],
+        ];
+    }
 }
