64595ac0cc2b70dffe39b31057707a401df3fc21
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-01-03 23:35:07 -0500

    feat: improve currency factory

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -6,19 +6,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CurrencyFactory extends Factory
 {
-    private array $names = [
-        'Afghan afghani',
-        'Brazilian real',
-        'Colombian peso',
-        'Dominican peso',
-        'United States dollar',
-        'Euro',
-    ];
-
     public function definition(): array
     {
         return [
-            'name' => $this->faker->randomElement($this->names),
+            'name' => $this->faker->words(2, true),
             'minor_unit' => 2,
             'alphabetic_code' => $this->faker->unique()->currencyCode(),
             'symbol' => '$',
