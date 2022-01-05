4acab523d1ddfd9879f8caf7a5fe08a7742cd7b4
juanco <juancoloradoalvarez@gmail.com>
2022-01-04 23:25:02 -0500

    fix: take the pr comments and modify the execute metodh

--- app/Actions/User/UserStoreAction.php
+++ app/Actions/User/UserStoreAction.php
@@ -3,28 +3,20 @@
 namespace App\Actions\User;
 
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 
 class UserStoreAction
 {
-    public static function execute(array $data, User $user): User
+    public static function execute(array $data): User
     {
-        $user->name = $data['name'];
-        $user->email = $data['email'];
-        $user->password = Hash::make($data['password']);
-        $user->created_by = auth()->id();
-        $user->updated_by = auth()->id();
-        self::isSaved($user);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'created_by' => auth()->id(),
+        ]);
 
-        return  $user;
+        return $user;
     }
 
-    public static function isSaved(User $user): bool
-    {
-        $user->save() ? event(new Registered($user)) : Log::error(trans(''));
-
-        return $user->save();
-    }
 }
