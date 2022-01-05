84452cff33502f38650af9d7436c6860e9605826
juanco <juancoloradoalvarez@gmail.com>
2022-01-04 23:22:45 -0500

    test: add and fix test for User  create and store Test

---
+++ tests/Feature/Users/CreateTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Users;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\Feature\Concerns\HasAuthenticatedUser;
+use Tests\Support\User\UserStoreDataProvider;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    use RefreshDatabase;
+    use UserStoreDataProvider;
+    use HasAuthenticatedUser;
+
+    private const USERS_ROUTE_NAME = 'users.create';
+
+    public function test_authenticated_user_can_see_the_create_view(): void
+    {
+        $this->actingAs($this->defaultUser())->get(route(self::USERS_ROUTE_NAME))
+            ->assertStatus(Response::HTTP_OK)
+            ->assertViewIs('users.create');
+    }
+
+    public function test_unauthenticated_will_be_redirect_to_login(): void
+    {
+        $this->get(route(self::USERS_ROUTE_NAME))
+            ->assertRedirect(route('login'));
+    }
+
+    public function test_it_shows_fields_to_create_users(): void
+    {
+        $this->actingAs($this->defaultUser())->get(route(self::USERS_ROUTE_NAME))
+            ->assertSee(trans('users.placeholders.name'))
+            ->assertSee(trans('users.placeholders.email'))
+            ->assertSee(trans('users.placeholders.password'))
+            ->assertSee(trans('users.placeholders.password_confirmation'));
+    }
+
+}
---
+++ tests/Feature/Users/StoreTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature\Users;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\Feature\Concerns\HasAuthenticatedUser;
+use Tests\Support\User\UserStoreDataProvider;
+use Tests\TestCase;
+
+class StoreTest extends TestCase
+{
+
+    use RefreshDatabase;
+    use UserStoreDataProvider;
+    use HasAuthenticatedUser;
+
+    public function test_it_can_create_a_user(): void
+    {
+        $data = $this->userData();
+        $this->actingAs($this->defaultUser())->post('/users', $data)
+            ->assertStatus(Response::HTTP_FOUND);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'created_by' => auth()->id()
+        ]);
+    }
+
+    /**
+     * @dataProvider invalidUserData
+     * @param string $field
+     * @param array  $data
+     */
+    public function test_it_can_not_create_a_user_with_invalid_data(string $field, array $data): void
+    {
+        $this->actingAs($this->defaultUser())
+            ->post('/users', $data)
+            ->assertStatus(Response::HTTP_FOUND)
+            ->assertInvalid([$field]);
+
+        $this->assertDatabaseMissing('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+        ]);
+    }
+
+    public function test_a_user_is_created_with_disabled_status_by_default(): void
+    {
+        $this->actingAs($this->defaultUser())->post('/users', $data = $this->userData())
+            ->assertRedirect('dashboard');
+
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'enabled_at' => null,
+        ]);
+    }
+
+    public function test_it_has_a_created_by_id_with_authenticated_user_id(): void
+    {
+
+        $this->actingAs($user = $this->defaultUser())
+            ->post(route('users.store'), $data = $this->userData())
+            ->assertRedirect('/dashboard');
+
+        $this->assertEquals( $user->id, User::latest('id')->first()->created_by);
+    }
+
+}
