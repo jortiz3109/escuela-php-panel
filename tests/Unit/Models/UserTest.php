c35161a74c0d3eaebb908183fc1c7a960a97983c
juanco <juancoloradoalvarez@gmail.com>
2022-01-05 07:39:50 -0500

    test: add UserTest for coverage

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_has_a_disabled_default_status()
+    {
+        $user = User::factory()->make();
+
+        $this->assertEquals(false, $user->isEnabled());
+
+    }
+
+}
