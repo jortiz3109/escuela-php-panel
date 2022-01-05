de32f571d2d698b60106297b7f1dccf869ef34e6
juanco <juancoloradoalvarez@gmail.com>
2022-01-05 06:38:21 -0500

    fix: Rename and change UserCreateViewModel

---
+++ app/ViewModels/Users/UserCreateViewModel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\ViewModels\Users;
+
+use App\Models\User;
+use App\ViewComponents\Inputs\EmailInput;
+use App\ViewComponents\Inputs\PasswordInput;
+use App\ViewComponents\Inputs\TextInput;
+use App\ViewModels\Concerns\HasCollection;
+use App\ViewModels\ViewModel;
+
+class CreateUsersViewModel extends ViewModel
+{
+    use HasCollection;
+
+    protected function buttons(): array
+    {
+        return [
+            'back' => [
+                'text' => trans('common.back'),
+                'route' => route('merchants.index'),
+            ],
+            'save' => [
+                'text' => trans('common.save'),
+            ],
+        ];
+    }
+
+    protected function title(): string
+    {
+        return trans('users.titles.create');
+    }
+
+    protected function fields(): array
+    {
+        return [
+            new TextInput(
+                trans('users.labels.name'),
+                trans('users.inputs.name'),
+                trans('users.placeholders.name'),
+                true
+            ),
+            new EmailInput(
+                trans('users.labels.email'),
+                trans('users.inputs.email'),
+                trans('users.placeholders.email'),
+            ),
+            new PasswordInput(
+                trans('users.labels.password'),
+                trans('users.inputs.password'),
+                trans('users.placeholders.password'),
+            ),
+            new PasswordInput(
+                trans('users.labels.password_confirmation'),
+                trans('users.inputs.password_confirmation'),
+                trans('users.placeholders.password_confirmation'),
+            )
+        ];
+
+    }
+
+    protected function data(): array
+    {
+        return [
+            'model' => new User(),
+            'action' => 'users.store',
+        ];
+    }
+
+}
