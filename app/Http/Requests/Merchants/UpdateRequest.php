fa01dc138ca91d8f9a6a907c427af85fab01ed57
Juan Ricardo Cardona <jucardonaa@unal.edu.co>
2022-01-05 18:33:07 -0500

    style: php cs fix

--- app/Http/Requests/Merchants/UpdateRequest.php
+++ app/Http/Requests/Merchants/UpdateRequest.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Requests\Merchants;
 
-use App\Rules\ValidUrlRule;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-
 class UpdateRequest extends StoreRequest
 {
 }
