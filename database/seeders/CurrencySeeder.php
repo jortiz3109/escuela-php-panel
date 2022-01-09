265f81e11f2ca3932d27c8767598fedfdceffbd1
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-01-08 20:35:15 -0500

    refactor: change currencies table insertions to chunks of rows

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -3,22 +3,23 @@
 namespace Database\Seeders;
 
 use App\Models\Currency;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class CurrencySeeder extends Seeder
 {
+    private const INSERT_CHUNK_SIZE = 50;
+
     public function run(): void
     {
-        $currencies = json_decode(file_get_contents(__DIR__ . '/../../resources/data_sources/currencies.json'), true);
+        $currencies = collect(
+            json_decode(file_get_contents(__DIR__ . '/../../resources/data_sources/currencies.json'), true)
+        )->map(function ($item) {
+            return $item + ['enabled_at' => Carbon::now()->format('Y-m-d')];
+        });
 
-        foreach ($currencies as $currency) {
-            Currency::create([
-                'name' => $currency['name'],
-                'minor_unit' => $currency['fraction_digits'],
-                'alphabetic_code' => $currency['currency_code'],
-                'symbol' => $currency['symbol'],
-                'enabled_at' => now(),
-            ]);
+        foreach ($currencies->chunk(self::INSERT_CHUNK_SIZE) as $chunk) {
+            Currency::insert($chunk->toArray());
         }
     }
 }
