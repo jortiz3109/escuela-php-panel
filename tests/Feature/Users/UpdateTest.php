ccf126cbc9c832b962e19d4694c2da4818df39bb
Juan David Zea <vikzea@gmail.com>
2022-01-10 17:34:39 -0500

    feat: Test validation

--- tests/Feature/Users/UpdateTest.php
+++ tests/Feature/Users/UpdateTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Users;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Tests\Feature\Concerns\HasAuthenticatedUser;
 use Tests\TestCase;
 
@@ -14,7 +15,6 @@ class UpdateTest extends TestCase
 
     public const USERS_ROUTE_NAME = 'users.update';
     private User $user;
-    private array $newData;
 
     protected function setUp(): void
     {
@@ -24,16 +24,11 @@ class UpdateTest extends TestCase
             'name' => 'John',
             'email' => 'test@email.com',
         ]);
-
-        $this->newData = [
-            'name' => 'new name',
-            'email' => 'new@email.com',
-        ];
     }
 
     public function test_a_guest_user_cannot_access(): void
     {
-        $response = $this->put(route(self::USERS_ROUTE_NAME, $this->user->id, $this->newData));
+        $response = $this->put(route(self::USERS_ROUTE_NAME, $this->user->id, $this->userData()));
         $response->assertRedirect(route('login'));
     }
 
@@ -41,12 +36,27 @@ class UpdateTest extends TestCase
     {
         $this
             ->actingAs($this->defaultUser())
-            ->put(route(self::USERS_ROUTE_NAME, $this->user->id), $this->newData);
+            ->put(route(self::USERS_ROUTE_NAME, $this->user->id), $this->userData());
 
         $this->user->refresh();
 
-        $this->assertEquals($this->newData['name'], $this->user->name);
-        $this->assertEquals($this->newData['email'], $this->user->email);
+        $this->assertEquals($this->userData()['name'], $this->user->name);
+        $this->assertEquals($this->userData()['email'], $this->user->email);
+    }
+
+    /**
+     * @param string $field
+     * @param array $data
+     * @return void
+     * @dataProvider userDataProvider
+     */
+    public function test_it_cannot_update_user_with_invalid_data(string $field, array $data): void
+    {
+        $response = $this
+            ->actingAs($this->defaultUser())
+            ->put(route(self::USERS_ROUTE_NAME, $this->user->id), $data);
+
+        $response->assertSessionHasErrors($field);
     }
 
     public function test_it_can_see_user_data(): void
@@ -54,5 +64,28 @@ class UpdateTest extends TestCase
         $response = $this->actingAs($this->defaultUser())->get(route(self::USERS_ROUTE_NAME, $this->user->id));
         $response->assertSee($this->user->name);
         $response->assertSee($this->user->email);
+    }
+
+    public function userDataProvider(): array
+    {
+        return [
+            'name is required' => ['field' => 'name', 'data' => array_merge($this->userData(), ['name' => null])],
+            'name has not numbers' => ['field' => 'name', 'data' => array_merge($this->userData(), ['name' => 'test1'])],
+            'name is greater than 2 chars' => ['field' => 'name', 'data' => array_merge($this->userData(), ['name' => 't'])],
+            'name is less than 255 chars' => ['field' => 'name', 'data' => array_merge($this->userData(), ['name' => Str::random(256)])],
+
+            'email is required' => ['field' => 'email', 'data' => array_merge($this->userData(), ['email' => null])],
+            'email is email' => ['field' => 'email', 'data' => array_merge($this->userData(), ['email' => 'notEmail'])],
+            'email is greater than 2 chars' => ['field' => 'email', 'data' => array_merge($this->userData(), ['email' => 't'])],
+            'email is less than 255 chars' => ['field' => 'email', 'data' => array_merge($this->userData(), ['email' => Str::random(256)])],
+        ];
+    }
+
+    public function userData(): array
+    {
+        return [
+            'name' => 'new name',
+            'email' => 'new@email.com',
+        ];
     }
 }
