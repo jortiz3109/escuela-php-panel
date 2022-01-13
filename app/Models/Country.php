9b3466ac41a1364637254182bf42e5576fe8b05b
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-01-12 22:25:20 -0500

    feat: seed countries from json file

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -10,5 +10,11 @@ class Country extends Model
     use HasFactory;
 
     public $timestamps = false;
-    protected $guarded = [];
+
+    protected $fillable = [
+        'name',
+        'alpha_two_code',
+        'alpha_three_code',
+        'numeric_code',
+    ];
 }
