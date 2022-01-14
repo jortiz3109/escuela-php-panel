fd303c5d65e3e5d30220f592c21890faa68d53ae
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-01-13 22:17:12 -0500

    feat: remove div in filters partials

--- resources/views/partials/filters/email.blade.php
+++ resources/views/partials/filters/email.blade.php
@@ -1,13 +1,11 @@
-<div class="column is-4">
-    <b-field
-        label="@lang('Email')"
-        type="{{ $errors->has('filters.email') ? 'is-danger' : '' }}"
-        message="{{ $errors->first('filters.email') }}">
-        <b-input
-            name="filters[email]"
-            value="{{ $value  }}"
-        ></b-input>
-    </b-field>
-</div>
+<b-field
+    label="@lang('Email')"
+    type="{{ $errors->has('filters.email') ? 'is-danger' : '' }}"
+    message="{{ $errors->first('filters.email') }}">
+    <b-input
+        name="filters[email]"
+        value="{{ $value  }}"
+    ></b-input>
+</b-field>
 
 
