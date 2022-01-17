1319967dbce29b1fd033e3eeb09dc08acd39c36a
Lauana Oliveira <lauanahubler@gmail.com>
2022-01-17 20:22:35 -0300

    test: fix

--- tests/Concerns/UserIndexDataProvider.php
+++ tests/Concerns/UserIndexDataProvider.php
@@ -11,13 +11,13 @@ trait UserIndexDataProvider
                 'enabled' => 5,
                 'disabled' => 15,
                 'filter_by' => 'enabled',
-                'filtered_data' => 8,
+                'filtered_data' => 15,
             ],
             'has filtered by disabled status' => [
                 'enabled' => 5,
                 'disabled' => 10,
                 'filter_by' => 'disabled',
-                'filtered' => 10,
+                'filtered' => 15,
             ],
         ];
     }
