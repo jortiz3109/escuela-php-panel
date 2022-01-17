8b1d547b7517da6aad65c5f88490ca640ebdea0f
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-01-17 13:34:34 -0500

    feat: remove duplicated lang file keys

--- resources/lang/en/users.php
+++ resources/lang/en/users.php
@@ -6,7 +6,7 @@ return [
         'create' => 'Register User',
     ],
     'navbar' => [
-        'title' => 'User',
+        'title' => 'Users',
     ],
     'inputs' => [
         'name' => 'name',
@@ -38,9 +38,6 @@ return [
         'index' => 'Users',
         'edit' => 'Edit user',
     ],
-    'navbar' => [
-        'title' => 'Users',
-    ],
     'fields' => [
         'name' => 'Name',
         'email' => 'Email',
@@ -50,10 +47,6 @@ return [
     'status' => [
         'enabled' => 'Enabled',
         'disabled' => 'Disabled',
-    ],
-    'placeholders' => [
-        'name' => 'Enter user name...',
-        'email' => 'example@email.com',
     ],
     'alerts' => [
         'successful_update' => 'The user has been updated',
